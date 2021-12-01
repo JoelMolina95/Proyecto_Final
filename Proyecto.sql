9be431c498cae8ec5ae7f35b48c56d2b5861033b
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2021-12-01 11:29:30 -0600

    update

--- Proyecto.sql
+++ Proyecto.sql
@@ -19,9 +19,6 @@ insert into Cliente values('Felix Ricardo','Ochoa Morales','Chalatenango','6a Av
 insert into Cliente values('Maria Eugenia','Benitez Lopez','Chalatenango','C. a Colonia San Francisco',56910429,'Maribntz@gmail.com')
 insert into Cliente values('Bryan Jose','Torres Rodriguez','Chalatenango','Av Liberta',39185029,'Bryantorres@gmail.com')
 
-select *from Cliente
-
-drop table Cliente
 
 create table Vehiculo(
 IdVehiculo integer primary key identity(1,1) not null,
@@ -30,47 +27,47 @@ modelo varchar(50) not null,
 color varchar (50) not null, 
 NumSerial Int not null,
 precio_venta money,
-stock integer not null,
+stock int not null,
 descripcion varchar(256) null,
 );
-drop table Vehiculo
+
 GO
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('BMW','M340i XDrive','azul', 652, $15000, 5, 'Motor 4 cilindros 2400 Vidrios electricos Combustible Desiel 4x4')
+VALUES ('Chevrolet ','Camaro SS','Negro', 985, $1148400, 3, '455 Caballos de potencia, Transmision automatica de 10 velocidades.')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Ford','Mustang GT','Rojo', 787, $27205, 3, 'Aceleración 0 a 100 km: 4 s')
+VALUES ('Ferrari','Enzo','Rojo', 814, $2640000, 1, 'Velocidad máxima: 336 km/h, Consumo de combustible: 3,4 km/l en ciudad y 5,1 km/l en ruta')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Chevrolet ','Camaro SS','Negro', 985, $1148400, 3, '455 CABALLOS DE POTENCIA, TRANSMISIÓN AUTOMÁTICADE 10 VELOCIDADES')
+VALUES ('Lamborghini ','Aventador Coupé','Verde', 352, $422290, 2, 'Velocidad máxima: 336 km/h, Consumo de combustible: 3,4 km/l en ciudad y 5,1 km/l en ruta')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Audi','R8','Azul', 652, $1150400, 2, 'Aceleración 0 a 100 km: 3.5 s')
+VALUES ('Ford','Mustang GT','Rojo', 787, $50205, 3, 'Tiene un motor de 533 caballos de potencia, tracción trasera y caja de cambios automática.')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Ferrari ','612 Scaglietti','Negro', 652, $240952, 2, 'Aceleración 0 a 100 km: 4.2 s.')
+VALUES ('Nissan ','GTR','Rojo', 592, $113540, 3, 'Motor V6 DOHC turbocargado doble de 3.8 litros y 24 válvulas, con transmisión integrante de eje transversal independiente')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Ferrari','Enzo','Rojo', 814, $2640000, 1, 'Aceleración 0 a 100 km: 3.3 s')
+VALUES ('Audi','R8','Azul', 652, $1150400, 2, 'El Audi R8 cuenta con un motor V10 de 5.2 litros con el que alcanza una velocidad máxima de 330 km/h. Sistema de frenado Nissan/Brembo')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Lamborghini ','Aventador Coupé','Verde', 352, $422290, 2, ' Aceleración 0 a 100 km: 2.9 s')
+VALUES ('Tesla ','Model 3 eléctrico','Rojo', 912, $49000, 4, 'Motor V6 DOHC turbocargado doble de 3.8 litros y 24 válvulas, con transmisión integrante de eje transversal independiente')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Nissan ','GTR','Rojo', 592, $113540, 3, 'Aceleración 0 a 100 km: 2.5 s')
+VALUES ('Ferrari ','612 Scaglietti','Negro', 652, $240952, 2, 'con un motor de gasolina, una cilindrada de 5,7 l y 540 CV de potencia. Caja de cambios automática de seis velocidades de tracción trasera, alcanza una velocidad de 320 km/h y acelerar de 0 a 100 km/h en 4,2 segundos.')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Tesla ','Model 3 eléctrico','Rojo', 912, $49000, 4, 'de 0 a 100km/h en 5,6 segundos')
+VALUES ('BMW','M340i XDrive','azul', 652, $15000, 5, 'Caja automática de ocho velocidades, con convertidor de par. Tracción integral permanente. Acelera de 0 a 100 km/h en 4,4 segundos y su velocidad máxima está limitada a 250 km/h.')
 
 INSERT INTO Vehiculo (Marca,modelo,color,NumSerial,precio_venta,stock,descripcion)
-VALUES ('Ford ','Mustang Shelby GT500','Azul', 789, $2415000, 1, 'transmisión de doble embrague Tremec TR-9070')
+VALUES ('Ford ','Mustang Shelby GT500','Azul', 789, $2415000, 1, 'Ford Shelby GT500 monta un potente Motor 5.2L V8 Supercargado de 760HP que entrega la aceleración más rápida en un Mustang de calle. ¡Ponte al volante y libera el poder de este icono de los Muscle Car Americanos!')
 
-select *from Vehiculo
+
 GO
 
 create table Venta(
-idVenta integer primary key identity(1,1) not null,
-idCliente integer not null,
+idVenta int primary key identity(1,1) not null,
+idCliente int not null,
 num_comprobante varchar (10) not null,
 fecha_hora datetime default getdate(),
 impuesto decimal (4,2) not null,
@@ -78,16 +75,14 @@ total decimal (11,2) not null,
 FOREIGN KEY (idCliente) REFERENCES Cliente (id_Cliente)
 );
 
-select *from Venta
-
 
 
 create table detalle_factura(
 idDetalle_venta integer primary key identity (1,1),
-idVenta integer not null,
-idArticulo integer not null,
-idPersona integer not null,
-cantidad integer not null,
+idVenta int not null,
+idArticulo int not null,
+idPersona int not null,
+cantidad int not null,
 precio decimal(11,2) not null,
 descuento decimal(11,2) not null,
 FOREIGN KEY (idVenta) REFERENCES Venta (idVenta),
@@ -95,5 +90,4 @@ FOREIGN KEY (idArticulo) REFERENCES Vehiculo (IdVehiculo),
 FOREIGN KEY (idPersona) REFERENCES Cliente (id_Cliente)
 );
 
-select *from detalle_factura
 
